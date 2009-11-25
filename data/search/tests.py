a4c644de2a8bc20485e115c95ec55ed946c14ab4
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2009-11-25 15:03:38 -0500

    Some prototype of the back-end for the search functionality. Nothing that's necessarily going to stay; just playing around for now.

---
+++ data/search/tests.py
@@ -0,0 +1,42 @@
+
+from datetime import date, datetime
+
+from django.test import TestCase
+
+from dcdata.contribution.models import Contribution
+from dcdata.models import Import
+from search.queries import DateSearch, ContributorSearch, RecipientSearch, build_query
+
+
+class SimpleTest(TestCase):
+    def create_contribution(self, **kwargs):
+        c = Contribution(**kwargs)
+        c.cycle='09'
+        c.transaction_namespace='urn:unittest:transaction'
+        c.import_reference=self.import_
+        c.save()
+        
+    def setUp(self):
+        print "Deleting database..."
+        Import.objects.all().delete()
+        Contribution.objects.all().delete()
+        
+        self.import_ = Import()
+        self.import_.save()
+        
+    def test_date(self):
+        self.create_contribution(datestamp=date(1999,12,25))
+        self.create_contribution(datestamp=date(1999,12,31))
+        self.create_contribution(datestamp=date(2000,1,1))
+        
+        q = build_query([[DateSearch.before_op, date(1999,12,31)]])
+        self.assertEqual(2, Contribution.objects.filter(*q).count())
+        
+        
+    def test_contributor(self):
+        self.create_contribution(datestamp=date(2000,1,1), contributor_entity='1234')
+        self.create_contribution(datestamp=date(2000,1,1), contributor_entity='1234')
+        self.create_contribution(datestamp=date(2000,1,1), contributor_entity='5678')
+        
+        q = build_query([[ContributorSearch.in_op, '1234']])
+        self.assertEqual(2, Contribution.objects.filter(*q).count())
