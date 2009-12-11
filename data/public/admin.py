4c9f44667e13f7b64a65959b5e11f727fe875426
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-11 16:18:13 -0500

    add API logging models and admin [#91 state:resolved]

--- data/public/admin.py
+++ data/public/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from dc_web.api.models import Key
+from dc_web.api.models import Key, APIInvocation
 from dc_web.public.models import UserProfile
 
 class KeyAdmin(admin.ModelAdmin):
     list_display = ('value','user','issued_on')
-    search_fields = ('value','user__username')
-    
+    search_fields = ('value','user__username')    
 admin.site.register(Key, KeyAdmin)
+
+class APIInvocationAdmin(admin.ModelAdmin):
+    list_display = ('user','total_records','crp_records','nimsp_records','query_string','timestamp')
+admin.site.register(APIInvocation, APIInvocationAdmin)
 
 class KeyInline(admin.TabularInline):
     model = Key
