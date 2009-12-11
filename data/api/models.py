20fc2533599e17f8f8918f41baafe380fd915788
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-11 16:18:13 -0500

    add API logging models and admin [#91 state:resolved]

--- data/api/models.py
+++ data/api/models.py
@@ -18,4 +18,17 @@ class Key(models.Model):
         if not self.value:
             self.value = uuid.uuid4().hex
         super(Key, self).save(**kwargs)
+
+
+class APIInvocation(models.Model):
+    user = models.ForeignKey(User, related_name="api_invocations")
+    timestamp = models.DateTimeField(default=datetime.datetime.utcnow)
+    method = models.CharField(max_length=128)
+    query_string = models.TextField()
+    total_records = models.IntegerField()
+    crp_records = models.IntegerField()
+    nimsp_records = models.IntegerField()
+    execution_time = models.IntegerField()
     
+    class Meta:
+        ordering = ('-timestamp',)
