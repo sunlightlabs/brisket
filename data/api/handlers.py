75118c5a090a94b3a5f0771e9307d19855ca975b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-11 16:19:49 -0500

    add logging, streaming emitters. still need some work [#92 state:open]

--- data/api/handlers.py
+++ data/api/handlers.py
@@ -1,7 +1,15 @@
+from django.db.models import Q
 from piston.handler import BaseHandler
-from matchbox.models import Entity
+from matchbox.models import Entity, Normalization
 from dcdata.contribution.models import Contribution
 from dc_web.search.contributions import CONTRIBUTION_SCHEMA
+from matchbox.queries import search_entities_by_name
+
+RESERVED_PARAMS = ('key','limit')
+
+#
+# contribution filter
+#
 
 class ContributionFilterHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -10,10 +18,16 @@ class ContributionFilterHandler(BaseHandler):
     
     def read(self, request):
         params = request.GET.copy()
-        if 'key' in params:
-            del params['key']
+        limit = int(request.GET.get('limit', 10))
+        for param in RESERVED_PARAMS:
+            if param in params:
+                del params[param]
         q = CONTRIBUTION_SCHEMA.extract_query(params)
-        return Contribution.objects.filter(*q)[:3]
+        return Contribution.objects.filter(*q)[:limit]
+
+#
+# entity handlers
+#
 
 class EntityHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -21,3 +35,16 @@ class EntityHandler(BaseHandler):
     
     def read(self, request, entity_id):
         return Entity.objects.get(pk=entity_id)
+
+class EntityFilterHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    fields = ('id','name','type','timestamp','reviewer',('attributes',('namespace','value')),('aliases',('alias',)))
+    model = Entity
+
+    def read(self, request):
+        qs = Entity.objects.all().select_related()
+        if 'name' in request.GET:
+            print search_entities_by_name(request.GET['name'])
+        if 'type' in request.GET:
+            qs = qs.filter(type=request.GET['type'])
+        return qs[:100]
--- data/api/urls.py
+++ data/api/urls.py
@@ -1,19 +1,26 @@
 from django.conf.urls.defaults import *
 from piston.emitters import Emitter
 from piston.resource import Resource
-from dc_web.api.handlers import ContributionFilterHandler, EntityHandler
+from dc_web.api.handlers import ContributionFilterHandler, EntityHandler, EntityFilterHandler
 from dc_web.api.auth import KeyAuthentication
-from dc_web.api.emitters import CSVEmitter
+from dc_web.api.emitters import StreamingLoggingCSVEmitter, StreamingLoggingJSONEmitter
 
-Emitter.register('csv', CSVEmitter, 'text/csv')
+Emitter.register('csv', StreamingLoggingCSVEmitter, 'text/csv')
+Emitter.register('json', StreamingLoggingJSONEmitter, 'application/json')
+Emitter.unregister('django')
+Emitter.unregister('pickle')
+Emitter.unregister('xml')
+Emitter.unregister('yaml')
 
 ad = { 'authentication': KeyAuthentication() }
 
 contributionfilter_handler = Resource(ContributionFilterHandler, **ad)
 entity_handler = Resource(EntityHandler, **ad)
+entityfilter_handler = Resource(EntityFilterHandler, **ad)
 
 urlpatterns = patterns('',
-    url(r'^contributions.(?P<emitter_format>.+)$', contributionfilter_handler, name='api_contributions'),
-    url(r'^entities/(?P<entity_id>\w+)(?P<emitter_format>.+)$', entity_handler, name='api_entities'),
+    url(r'^contributions.(?P<emitter_format>.+)$', contributionfilter_handler, name='api_contributions_filter'),
+    url(r'^entities/(?P<entity_id>\w+).(?P<emitter_format>.+)$', entity_handler, name='api_entities'),
+    url(r'^entities.(?P<emitter_format>.+)$', entityfilter_handler, name='api_entities_filter'),
 )
 
