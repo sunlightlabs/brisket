62fefbbf546479fd47f8d447fca6e5ee1e013a9f
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-01-04 17:24:09 -0500

    add new views and urls for templates

--- data/public/urls.py
+++ data/public/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('dc_web.public.views',
     url(r'^api/$', 'api_index', name="api_index"),
     url(r'^bulk/$', 'bulk_index', name="bulk_index"),
+    url(r'^docs/$', 'doc_index', name="doc_index"),
+    url(r'^filter/$', 'filter', name="filter"),
+    url(r'^data/contributions/$', 'data_contributions', name="data_contributions"),
+    url(r'^data/contributions/download/$', 'data_contributions_download', name="data_contributions_download"),
     url(r'^$', 'index', name="index"),
 )
