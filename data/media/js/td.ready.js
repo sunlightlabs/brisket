35f8a2eb0f0dc75733058c780346b428aa93b036
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-02-25 13:32:50 -0500

    fix wording and add employer field

--- data/media/js/td.ready.js
+++ data/media/js/td.ready.js
@@ -27,9 +27,33 @@ $().ready(function() {
     TD.DataFilter.registerFilter({
         name: 'contributor_ft',
         label: 'Contributor',
-        help: 'Name of individual or PAC that made contribution',
+        help: 'Name of individual, employer, or organization that made contribution',
         field: TD.DataFilter.TextField,
         allowMultipleFields: true,
+    });
+
+    TD.DataFilter.registerFilter({
+        name: 'contributor_state',
+        label: 'Contributor State',
+        help: 'State from which the contribution was made',
+        field: TD.DataFilter.DropDownField,
+        allowMultipleFields: true,
+        options: [
+            ['AL', 'Alabama'],          ['AK', 'Alaska'],       ['AZ', 'Arizona'],      ['AR', 'Arkansas'],
+            ['CA', 'California'],       ['CO', 'Colorado'],     ['CT', 'Connecticut'],  ['DE', 'Delaware'],
+            ['DC', 'District of Columbia'],
+            ['FL', 'Florida'],          ['GA', 'Georgia'],      ['HI', 'Hawaii'],       ['ID', 'Idaho'],
+            ['IL', 'Illinois'],         ['IN', 'Indiana'],      ['IA', 'Iowa'],         ['KS', 'Kansas'],
+            ['KY', 'Kentucky'],         ['LA', 'Louisiana'],    ['ME', 'Maine'],        ['MD', 'Maryland'],
+            ['MA', 'Massachusetts'],    ['MI', 'Michigan'],     ['MN', 'Minnesota'],    ['MS', 'Mississippi'],
+            ['MO', 'Missouri'],         ['MT', 'Montana'],      ['NE', 'Nebraska'],     ['NV', 'Nevada'],
+            ['NH', 'New Hampshire'],    ['NJ', 'New Jersey'],   ['NM', 'New Mexico'],   ['NY', 'New York'],
+            ['NC', 'North Carolina'],   ['ND', 'North Dakota'], ['OH', 'Ohio'],         ['OK', 'Oklahoma'],
+            ['OR', 'Oregon'],           ['PA', 'Pennsylvania'], ['RI', 'Rhode Island'], ['SC', 'South Carolina'],
+            ['SD', 'South Dakota'],     ['TN', 'Tennessee'],    ['TX', 'Texas'],        ['UT', 'Utah'],
+            ['VT', 'Vermont'],          ['VA', 'Virginia'],     ['WA', 'Washington'],   ['WV', 'West Virginia'],
+            ['WI', 'Wisconsin'],        ['WY', 'Wyoming']
+        ]
     });
     
     TD.DataFilter.registerFilter({
@@ -37,6 +61,13 @@ $().ready(function() {
         label: 'Date',
         help: 'This is the date of the contribution',
         field: TD.DataFilter.DateRangeField
+    });
+    
+    TD.DataFilter.registerFilter({
+        name: 'employer_ft',
+        label: 'Employer',
+        help: 'Name of employer or organization associated with contribution',
+        field: TD.DataFilter.TextField,
     });
     
     TD.DataFilter.registerFilter({
@@ -80,30 +111,6 @@ $().ready(function() {
         help: 'Name of candidate or PAC that received contribution',
         field: TD.DataFilter.TextField,
         allowMultipleFields: true,
-    });
-    
-    TD.DataFilter.registerFilter({
-        name: 'state',
-        label: 'State',
-        help: 'State from which the contribution was made',
-        field: TD.DataFilter.DropDownField,
-        allowMultipleFields: true,
-        options: [
-            ['AL', 'Alabama'],          ['AK', 'Alaska'],       ['AZ', 'Arizona'],      ['AR', 'Arkansas'],
-            ['CA', 'California'],       ['CO', 'Colorado'],     ['CT', 'Connecticut'],  ['DE', 'Delaware'],
-            ['DC', 'District of Columbia'],
-            ['FL', 'Florida'],          ['GA', 'Georgia'],      ['HI', 'Hawaii'],       ['ID', 'Idaho'],
-            ['IL', 'Illinois'],         ['IN', 'Indiana'],      ['IA', 'Iowa'],         ['KS', 'Kansas'],
-            ['KY', 'Kentucky'],         ['LA', 'Louisiana'],    ['ME', 'Maine'],        ['MD', 'Maryland'],
-            ['MA', 'Massachusetts'],    ['MI', 'Michigan'],     ['MN', 'Minnesota'],    ['MS', 'Mississippi'],
-            ['MO', 'Missouri'],         ['MT', 'Montana'],      ['NE', 'Nebraska'],     ['NV', 'Nevada'],
-            ['NH', 'New Hampshire'],    ['NJ', 'New Jersey'],   ['NM', 'New Mexico'],   ['NY', 'New York'],
-            ['NC', 'North Carolina'],   ['ND', 'North Dakota'], ['OH', 'Ohio'],         ['OK', 'Oklahoma'],
-            ['OR', 'Oregon'],           ['PA', 'Pennsylvania'], ['RI', 'Rhode Island'], ['SC', 'South Carolina'],
-            ['SD', 'South Dakota'],     ['TN', 'Tennessee'],    ['TX', 'Texas'],        ['UT', 'Utah'],
-            ['VT', 'Vermont'],          ['VA', 'Virginia'],     ['WA', 'Washington'],   ['WV', 'West Virginia'],
-            ['WI', 'Wisconsin'],        ['WY', 'Wyoming']
-        ]
     });
 
     TD.DataFilter.init();
