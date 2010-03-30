966338c6f76c44d21220c9b80bbbd7f93f92e6d5
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-03-30 16:48:15 -0400

    make api execution time logging work

--- data/public/admin.py
+++ data/public/admin.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 from dcapi.models import Invocation
 
 class InvocationAdmin(admin.ModelAdmin):
-    list_display = ('caller_key','total_records','crp_records','nimsp_records','query_string','timestamp')
+    list_display = ('caller_key','crp_records','nimsp_records','query_string','execution_time','timestamp')
 admin.site.register(Invocation, InvocationAdmin)
