405279c332f7a8e27ee6f324fa4dcf62148d6f6b
jessykate <jessy.cowansharp@gmail.com>
2010-04-03 20:42:34 -0400

    building out politician template and view

--- influence/views.py
+++ influence/views.py
@@ -9,13 +9,20 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
 from django.template import RequestContext
-
-from brisket.influence.forms import SearchForm
+from brisket.influence.forms import SearchForm, ElectionCycle
 from lib import AggregatesAPI, InfluenceNetworkBuilder
 
+def brisket_context(request): 
+    return RequestContext(request, {'search_form': SearchForm(), 
+                                    })
+
+def entity_context(request): 
+    return RequestContext(request, {'search_form': SearchForm(), 
+                                    'cycle_form': ElectionCycle(), 
+                                    })
+
 def index(request):    
-    search_form = SearchForm()
-    return render_to_response('index.html', {'form':search_form})
+    return render_to_response('index.html', brisket_context(request))
 
 def search(request):
     if not request.GET.get('query', None):
@@ -27,7 +34,8 @@ def search(request):
         api = AggregatesAPI()
         results = api.entity_search(submitted_form.cleaned_data['query'])
         entity_results = [result for result in results if result['id']]
-        return render_to_response('results.html', {'results': entity_results})
+        return render_to_response('results.html', {'results': entity_results}, 
+                                  brisket_context(request))
     else: 
         form = SearchForm(request.GET)
         return HttpResponseRedirect('/')
@@ -49,49 +57,49 @@ def organization_entity(request, entity_id):
     #contributions_barchart = topn_barchart(top_contributors)
     #recipients_barchart = topn_barchart(top_recipients)
 
-    return render_to_response('organization.html', {'entity_id': entity_id, 
-                                                    'entity_info': entity_info,
-                                                    'top_contributors': top_contributors, 
-                                                    'top_recipients': top_recipients, 
-                                                    'pie_chart' : pie_chart,
-     #                                               'contributions_barchart': contributions_barchart,
-     #                                               'recipients_barchart': recipients_barchart,
-                                                    })
+    return render_to_response('organization.html', 
+                              {'entity_id': entity_id, 
+                               'entity_info': entity_info,
+                               'top_contributors': top_contributors, 
+                               'top_recipients': top_recipients, 
+                               'pie_chart' : pie_chart,
+                               },
+                              entity_context(request))
 
 def politician_entity(request, entity_id):
-
     api = AggregatesAPI()    
     entity_info = api.entity_metadata(entity_id)
-    top_contributors = api.top_contributors(entity_id)
     # politicians only receive contributions
-
+    top_contributors = api.top_contributors(entity_id)
     fake_piechart_instate = {
         'In State' : str(32.7),
         'Out of State' : str(100.0-32.7)
         }
-    pie_chart_instate = breakdown_piechart(fake_piechart_instate, "In State vs. Out of State")
-
+    pie_chart_instate = breakdown_piechart(fake_piechart_instate, 
+                                           "In State vs. Out of State")
     fake_piechart_source = {
         'Individuals' : str(17.7),
         'PACs' : str(100.0-17.7)
         }
     pie_chart_source = breakdown_piechart(fake_piechart_source, "Individuals vs. PACs")
-
     contributions_barchart = topn_barchart(top_contributors)
-
-    return render_to_response('politician.html', {'entity_id': entity_id,
-                                                  'entity_info': entity_info,
-                                                  'top_contributors': top_contributors,
-                                                  'pie_chart_instate' : pie_chart_instate,
-                                                  'pie_chart_source' : pie_chart_source,
-                                                  'contributions_barchart': contributions_barchart,
-                                                  })
+    amounts = [str(contributor['amount']) for contributor in top_contributors]
+    sparkline = timeline_sparkline(amounts)
+    return render_to_response('politician.html', 
+                              {'entity_id': entity_id,
+                               'entity_info': entity_info,
+                               'top_contributors': top_contributors,
+                               'pie_chart_instate' : pie_chart_instate,
+                               'pie_chart_source' : pie_chart_source,
+                               'contributions_barchart': contributions_barchart,
+                               'sparkline': sparkline,
+                               },
+                              entity_context(request))
         
-def individual_entity(request, entity_id):
-    
+def individual_entity(request, entity_id):    
     # individuals only give contributions
-
-    return render_to_response('individual.html', {'entity_id': entity_id})
+    return render_to_response('individual.html', {'entity_id': entity_id},
+                              entity_context(request))
 
 def topn_barchart(data_list):
     ''' generates a horizontal bargraph of aggregate contribution data. '''
@@ -126,4 +134,10 @@ def breakdown_piechart(data_dict, title=None):
 def timeline_sparkline(data_list):
     ''' generates a sparkline of contributions given or received over
     the time period represented by the '''
-    pass
+    if not len(data_list):
+        return ""
+    max_value = max([float(item) for item in data_list])
+    scaling = "&chds=0,%f" % max_value
+    data = "&chd=t:%s" % ','.join(data_list)
+    url = "http://chart.apis.google.com/chart?cht=ls&chs=100x30"+data+scaling
+    return url
