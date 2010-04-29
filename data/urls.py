a41bf91c3376142fb1b1dd691870cb69e396309b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-04-29 09:56:46 -0400

    remove django-registration

--- data/urls.py
+++ data/urls.py
@@ -2,25 +2,13 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
-from registration.views import activate
-from registration.views import register
-from dc_web.public.forms import CustomRegistrationForm
 
 admin.autodiscover()
 
-# registration URLs
 urlpatterns = patterns('',
-    url(r'^accounts/register/$', register, {
-            'form_class': CustomRegistrationForm,
-            'backend': 'registration.backends.default.DefaultBackend'
-        }, name='registration_register'),
-)
-
-urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/1.0/', include('dcapi.urls')),
     url(r'^api/locksmith/', include('locksmith.auth.urls')),
-    url(r'^accounts/', include('registration.urls')),
     url(r'^', include('dc_web.public.urls')),
 )
 
