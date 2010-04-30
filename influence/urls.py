acb4dded5e10a9fd619cba6d51a3d72bc0d7a5d2
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-04-30 13:44:05 -0400

    Changed URL regexes to use '\w' instead of '[a-z]'. Hyphens now allowed.

--- influence/urls.py
+++ influence/urls.py
@@ -6,21 +6,21 @@ urlpatterns = patterns('brisket.influence.views',
     url(r'^search', 'search', name='search'),                       
 
     # detail pages                       
-    url(r'^politician/[a-z\-]+/(?P<entity_id>\w+)/details', 'industry_detail', 
+    url(r'^politician/[\w\-]+/(?P<entity_id>\w+)/details', 'industry_detail', 
         name='industry_detail'),                       
-    url(r'^organization/[a-z\-]+/(?P<entity_id>\w+)/details', 'industry_detail', 
+    url(r'^organization/[\w\-]+/(?P<entity_id>\w+)/details', 'industry_detail', 
         name='industry_detail'),                 
 
     # industry                       
-    url(r'^sector/[a-z\-]+/(?P<entity_id>\w+)', 'sector_detail', 
+    url(r'^sector/[\w\-]+/(?P<entity_id>\w+)', 'sector_detail', 
         name='sector_detail'),                       
 
     # entity pages
-    url(r'^organization/[a-z\-]+/(?P<entity_id>\w+)', 'organization_entity', 
+    url(r'^organization/[\w\-]+/(?P<entity_id>\w+)', 'organization_entity', 
         name='organization_entity'),                       
-    url(r'^politician/[a-z\-]+/(?P<entity_id>\w+)', 'politician_entity', 
+    url(r'^politician/[\w\-]+/(?P<entity_id>\w+)', 'politician_entity', 
         name='politician_entity'),                       
-    url(r'^individual/[a-z\-]+/(?P<entity_id>\w+)', 'individual_entity', 
+    url(r'^individual/[\w\-]+/(?P<entity_id>\w+)', 'individual_entity', 
         name='individual_entity'),                       
 
     # utility
