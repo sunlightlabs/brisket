a922c21f7278d70a2b06bdf64131a71d70c753d5
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-05-06 17:29:36 -0400

    Every API method now being tested.

--- influence/tests.py
+++ influence/tests.py
@@ -1,23 +1,87 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
 
-Replace these with more appropriate tests for your application.
-"""
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from influence.api import AggregatesAPI
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+api = AggregatesAPI()
+PELOSI = 'ff96aa62d48f48e5a1e284efe74a0ba8'
+PICKENS = '945bcd0635bc434eacb7abcdcd38abea'
+BANKERS = 'c039e8a46406458fbd3d48fd174554fd'
+CYCLE = 2008
+LIMIT = 10
 
+
+# todo: inconsistent use of cycle and limit parameters.
+# todo: remove all 'type' parameters
+# todo: remove keyword args, or at least use them consistently
+class ContributionsAPITests(TestCase):
+
+    def test_pol_contributors(self):
+        results = api.pol_contributors(PELOSI, 'unused field?', CYCLE)
+        self.assertEqual(10, len(results))
+        
+    def test_pol_breakdown(self):
+        results = api.pol_breakdown(PELOSI, 'local', CYCLE)
+        self.assertEqual(2, len(results))
+
+        results = api.pol_breakdown(PELOSI, 'entity', CYCLE)
+        self.assertEqual(2, len(results))
+
+    def test_indiv_recipients(self):
+        results = api.indiv_recipients(PICKENS, 'org', CYCLE)
+        self.assertEqual(6, len(results))
+        
+        results = api.indiv_recipients(PICKENS, 'pol', CYCLE)
+        self.assertEqual(10, len(results))
+        
+    def test_indiv_breakdown(self):
+        results = api.indiv_breakdown(PICKENS, 'party', CYCLE)
+        self.assertEqual(1, len(results))
+        
+    def test_org_recipients(self):
+        results = api.org_recipients(BANKERS, cycle=CYCLE, limit=LIMIT)
+        self.assertEqual(10, len(results))
+        
+    def test_org_breakdown(self):
+        results = api.org_breakdown(BANKERS, 'party', CYCLE)
+        self.assertEqual(2, len(results))
+        
+        results = api.org_breakdown(BANKERS, 'level', CYCLE)
+        self.assertEqual(1, len(results))
+        
+    # todo: rename to have org/pol/indiv prefix
+    def test_top_sectors(self):
+        results = api.top_sectors(PELOSI, cycle=CYCLE, limit=LIMIT)
+        self.assertEqual(10, len(results))
+        
+    # todo: rename
+    # todo: seems completely broken
+    def test_contributions_by_sector(self):
+        results = api.contributions_by_sector(PELOSI, 'H')
+        
+
+class EntityAPITests(TestCase):
+    
+    def test_entity_search(self):
+        results = api.entity_search('pelosi')
+        self.assertEqual(2, len(results))
+        
+    def test_entity_metadata(self):
+        results = api.entity_metadata(PELOSI, CYCLE)
+        self.assertEqual(5, len(results))    
+
+# rename to consistent scheme
+class LobbyingAPITests(TestCase):
+    
+    def test_lobbying_for_org(self):
+        results = api.lobbying_for_org(BANKERS, CYCLE)
+        self.assertEqual(10, len(results))
+
+    def test_issues_lobbied_for(self):
+        results = api.issues_lobbied_for(BANKERS, CYCLE)
+        self.assertEqual(10, len(results))
+        
+        
+        
