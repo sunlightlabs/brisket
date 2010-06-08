1994c6b6d2fb639126ee6669e55a2920f7fcbc7a
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-07 23:41:58 -0400

    add axis lines to bar chart and remove stroke on pie chart

--- media/js/brisket_charts.js
+++ media/js/brisket_charts.js
@@ -42,7 +42,8 @@ function piechart(div, data, type) {
     pie = r.g.piechart(70, 70, 60, values, {
         legend: labels,
         legendpos: "east",
-        colors: colors
+        colors: colors,
+        strokewidth: 0,
     });
 
     var lbl = undefined;
@@ -89,53 +90,53 @@ function barchart(div, data, limit) {
     b = Raphael(div);
     b.g.txtattr.font = "11px 'Fontin Sans', Fontin-Sans, sans-serif";
 
-  if (limit && limit < data.length) {
-    data= data.slice(0,limit);
-  }
-
-  /* if the data has less than 10 records, pad it so that
-   * the chart doesn't look like crap. */
-  var original_len = data.length;
-   if (data.length < 10) {
-    for (var i=data.length; i<10; i++) {
-      data[i] = {'key':' ', 'value': 0, 'href':'#'};
-    }
-   }
-
-    data_values = [];
-    for (var i = 0; i < data.length; i++) {
-	data_values.push(data[i]['value']);
+    if (limit && limit < data.length) {
+        data = data.slice(0, limit);
     }
 
-    data_labels = [];
-    for (var i = 0; i < data.length; i++) {
-	data_labels.push(data[i]['key']);
+    /* if the data has less than 10 records, pad it so that
+    * the chart doesn't look like crap. */
+    var original_len = data.length;
+    if (data.length < 10) {
+        for (var i=data.length; i<10; i++) {
+            data[i] = {'key':' ', 'value': 0, 'href':'#'};
+        }
     }
 
-    data_hrefs = [];
+    var data_values = [];
     for (var i = 0; i < data.length; i++) {
-	data_hrefs.push(data[i]['href']);
+	    data_values.push(data[i]['value']);
+    }
+
+    var data_labels = [];
+    for (var i = 0; i < data.length; i++) {
+	    data_labels.push(data[i]['key']);
+    }
+
+    var data_hrefs = [];
+    for (var i = 0; i < data.length; i++) {
+	    data_hrefs.push(data[i]['href']);
     }
 
     opts = {
-    "type": "soft",
-    "gutter": 30, //space between bars, as fn of bar width/height
-    "stacked": false,
-    "colors" : ["#EFCC01"]
+        "type": "soft",
+        "gutter": 30, //space between bars, as fn of bar width/height
+        "stacked": false,
+        "colors" : ["#EFCC01"]
     };
 
     /* data array must be passed inside another array-- barchart fn
        supports multiple data series so expects an array of arrays,
        even for just one data series. Else it will treat each data
        point as one series. */
-  var barchart = b.g.hbarchart(10,10, 350, 150, [data_values], opts);
+    var barchart = b.g.hbarchart(10,10, 330, 150, [data_values], opts);
 
     // pass in labels array inside another array
     barchart.label([data_labels], false);
 
     // add links to the labels
-    for (var i=0; i< barchart.labels.length; i++) {
-      barchart.labels[i].attr({'href': data_hrefs[i] })
+    for (var i = 0; i < barchart.labels.length; i++) {
+        barchart.labels[i].attr({'href': data_hrefs[i] });
     }
 
     // this is the desired link colour, but it also seems to make
@@ -155,11 +156,12 @@ function barchart(div, data, limit) {
      chart. */
     var far_right = 0;
     for (var i = 0; i < data_labels.length; i++) {
-	bb = barchart.labels[i].getBBox();
+        var bb = barchart.labels[i].getBBox();
         if (bb.x + bb.width > far_right) {
             far_right = bb.x + bb.width;
         }
     }
+    far_right = 165; // FAKE IT HERE!!
     barchart.translate(far_right);
 
     /* add text markers for the amounts (which unfortunately uses a
@@ -167,13 +169,21 @@ function barchart(div, data, limit) {
     s = b.set();
     for (var i=0; i< original_len; i++) {
         x = barchart.bars[0][i].x;
-	y = barchart.bars[0][i].y;
+	y = barchart.bars[0][i].y + 1;
 	text = '$'+barchart.bars[0][i].value;
 	marker = b.g.text(x,y,text);
 	s.push(marker);
     };
     var spacing = 10; // spacing between bars and text markers
     s.attr({translation: far_right+spacing, 'text-anchor': 'start'});
+    
+    var yAxis = b.path("M 175 10 L 175 155");
+    yAxis.attr({"stroke": "#827D7D", "stroke-width": 1});
+    yAxis.show();
+    
+    var xAxis = b.path("M 175 155 L 580 155");
+    xAxis.attr({"stroke": "#827D7D", "stroke-width": 1});
+    xAxis.show();
 
 }
 
