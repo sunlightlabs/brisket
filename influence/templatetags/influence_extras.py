35cb6607e31b9a130a4a8a666fd39807ddbac95f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-06-22 15:03:34 -0400

    Put readable labels on the seat column.

--- influence/templatetags/influence_extras.py
+++ influence/templatetags/influence_extras.py
@@ -9,3 +9,19 @@ register = template.Library()
 def standardize_politician_name_filter(name):
     return helpers.standardize_politician_name(name)
 
+
+
+seat_labels = {'federal:senate': 'Senate',
+               'federal:house': 'House',
+               'federal:president': 'President',
+               'state:upper': 'State Upper Chamber',
+               'state:lower': 'State Lower Chamber',
+               'state:governor': 'State Governor',
+               'state:judicial': 'State Judiciary',
+               'state:office': 'Other State Office'
+               }
+
+@register.filter(name='seat_label')
+@stringfilter
+def seat_label_filter(raw):
+    return seat_labels.get(raw, raw)
