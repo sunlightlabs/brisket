d53d6af23eb13910e1d13c982f944a5a6d6de914
jessykate <jessy.cowansharp@gmail.com>
2010-06-23 15:08:42 -0400

    moving helper functions to the end of views.py

--- influence/views.py
+++ influence/views.py
@@ -81,26 +81,6 @@ def search(request):
     else:
         return HttpResponseRedirect('/')
 
-def _amt_given_decreasing(d1, d2):
-    ''' a cmp function for sort(), to sort dicts by increasing value
-    of the total_given item'''
-
-    if float(d1['total_given']) > float(d2['total_given']):
-        return -1
-    if float(d1['total_given']) < float(d2['total_given']):
-        return 1
-    else: return 0
-
-def _amt_received_decreasing(d1, d2):
-    ''' a cmp function for sort(), to sort dicts by increasing value
-    of the total_given item'''
-
-    if float(d1['total_received']) > float(d2['total_received']):
-        return -1
-    if float(d1['total_received']) < float(d2['total_received']):
-        return 1
-    else: return 0
-
 def organization_landing(request):
     context = {}
     context['top_n_organizations'] = api.top_n_organizations(cycle=LATEST_CYCLE, limit=50)
@@ -282,69 +262,6 @@ def politician_entity(request, entity_id):
     return render_to_response('politician.html', context,
                               entity_context(request, cycle, metadata['available_cycles']))
 
-def _barchart_href(record, cycle, entity_type):
-    if 'recipient_entity' in record.keys():
-        if record['recipient_entity']:
-            href = str("/%s/%s/%s?cycle=%s" % (entity_type, slugify(record['recipient_name']),
-                                               record['recipient_entity'], cycle))
-        else:
-            href = -1
-
-    elif 'id' in record.keys():
-        if record['id']:
-            href = str("/%s/%s/%s?cycle=%s" % (entity_type, slugify(record['name']),
-                                               record['id'], cycle))
-        else:
-            href = -1
-    else:
-        href = -1
-
-    return href
-
-def _generate_label(string):
-    ''' truncate names longer than max_length and normalize the case
-    to use title case'''
-    max_length = 27
-    label = string[:max_length] + (lambda x, l: (len(x)>l and "...")
-                                   or "")(string, max_length)
-    return label.title()
-
-
-def get_metadata(entity_id, cycle, entity_type):
-    ''' beginnings of some refactoring. half implemented but
-    harmless. do not pet or feed.'''
-    data = {}
-    # check the metadata to see which of the fields are present. this
-    # determines which sections to display on the entity page.
-    section_indicators = {'individual': {'contributions': ('contributor_amount',),
-                                         'lobbying': ('lobbying_count',)},
-                          'organization' : {'contributions' : ('contributor_amount',),
-                                            'lobbying': ('lobbying_count',)},
-                          'politician' : {'contributions' : ('recipient_amount',)}
-                          }
-
-    entity_info = api.entity_metadata(entity_id, cycle)
-
-    # check which types of data are available about this entity
-    print 'getting metadata for %s %s' % (entity_type, entity_id)
-    for data_type, indicators in section_indicators[entity_type].iteritems():
-        try:
-            print 'totals for %s' % indicators[0]
-            print entity_info['totals'][cycle][indicators[0]]
-        except: pass
-        if (entity_info['totals'].get(cycle, False) and
-            [True for ind in indicators if entity_info['totals'][cycle][ind] ]):
-            data[data_type] = True
-        else:
-            data[data_type] = False
-
-    data['available_cycles'] = entity_info['totals'].keys()
-    # discard the info from cycles that are not the current one
-    if entity_info['totals'].get(cycle, None):
-        entity_info['totals'] = entity_info['totals'][cycle]
-    data['entity_info'] = entity_info
-
-    return data
 
 
 def individual_entity(request, entity_id):
@@ -435,39 +352,6 @@ def industry_detail(request, entity_id):
                                },
                               entity_context(request, cycle))
 
-def bar_validate(data):
-    ''' take a dict formatted for submission to the barchart
-     generation function, and make sure there's data worth displaying.
-     if so, return the original data. if not, return false.'''
-    print 'original bar data to be validated'
-    print data
-
-    positive_data = [d for d in data if int(float(d['value'])) > 0]
-    data = positive_data
-    # if all the data is 0 or if the list with only positive data is
-    # empty, return false
-    if sum([int(float(record['value'])) for record in data]) == 0:
-        return False
-    else:
-        return data
-
-def pie_validate(data):
-    ''' take a dict formatted for submission to the piechart
-     generation function, and make sure there's data worth displaying.
-     if so, return the original data. if not, return false.'''
-
-    print 'original pie data to be validated'
-    print data
-    
-    positive = {}
-    for k,v in data.iteritems():
-        if int(float(v)) != 0:
-            positive[k] = v
-    if len(positive) == 0:
-        return False
-    else:
-        return positive
-    
 
 # lobbying
 def lobbying_by_industry(lobbying_data):
@@ -518,6 +402,34 @@ def lobbying_by_firm(lobbying_data):
     z.sort(_tuple_cmp, reverse=True) # stupid in place sorting
     return z
 
+# random util/helper functions
+
+def slugify(string):
+    ''' like the django template tag, converts to lowercase, removes
+    all non-alphanumeric characters and replaces spaces with
+    hyphens. '''
+    return re.sub(" ", "-", re.sub("[^a-zA-Z0-9 -]+", "", string)).lower()
+
+def _amt_given_decreasing(d1, d2):
+    ''' a cmp function for sort(), to sort dicts by increasing value
+    of the total_given item'''
+
+    if float(d1['total_given']) > float(d2['total_given']):
+        return -1
+    if float(d1['total_given']) < float(d2['total_given']):
+        return 1
+    else: return 0
+
+def _amt_received_decreasing(d1, d2):
+    ''' a cmp function for sort(), to sort dicts by increasing value
+    of the total_given item'''
+
+    if float(d1['total_received']) > float(d2['total_received']):
+        return -1
+    if float(d1['total_received']) < float(d2['total_received']):
+        return 1
+    else: return 0
+
 def _tuple_cmp(t1, t2):
     ''' a cmp function for sort(), to sort tuples by increasing value
     of the tuple's 2nd item (index 1)'''
@@ -527,9 +439,99 @@ def _tuple_cmp(t1, t2):
         return 1
     else: return 0
 
+def bar_validate(data):
+    ''' take a dict formatted for submission to the barchart
+     generation function, and make sure there's data worth displaying.
+     if so, return the original data. if not, return false.'''
+    print 'original bar data to be validated'
+    print data
 
-def slugify(string):
-    ''' like the django template tag, converts to lowercase, removes
-    all non-alphanumeric characters and replaces spaces with
-    hyphens. '''
-    return re.sub(" ", "-", re.sub("[^a-zA-Z0-9 -]+", "", string)).lower()
+    positive_data = [d for d in data if int(float(d['value'])) > 0]
+    data = positive_data
+    # if all the data is 0 or if the list with only positive data is
+    # empty, return false
+    if sum([int(float(record['value'])) for record in data]) == 0:
+        return False
+    else:
+        return data
+
+def pie_validate(data):
+    ''' take a dict formatted for submission to the piechart
+     generation function, and make sure there's data worth displaying.
+     if so, return the original data. if not, return false.'''
+
+    print 'original pie data to be validated'
+    print data
+    
+    positive = {}
+    for k,v in data.iteritems():
+        if int(float(v)) != 0:
+            positive[k] = v
+    if len(positive) == 0:
+        return False
+    else:
+        return positive
+    
+def _barchart_href(record, cycle, entity_type):
+    if 'recipient_entity' in record.keys():
+        if record['recipient_entity']:
+            href = str("/%s/%s/%s?cycle=%s" % (entity_type, slugify(record['recipient_name']),
+                                               record['recipient_entity'], cycle))
+        else:
+            href = -1
+
+    elif 'id' in record.keys():
+        if record['id']:
+            href = str("/%s/%s/%s?cycle=%s" % (entity_type, slugify(record['name']),
+                                               record['id'], cycle))
+        else:
+            href = -1
+    else:
+        href = -1
+
+    return href
+
+def _generate_label(string):
+    ''' truncate names longer than max_length and normalize the case
+    to use title case'''
+    max_length = 27
+    label = string[:max_length] + (lambda x, l: (len(x)>l and "...")
+                                   or "")(string, max_length)
+    return label.title()
+
+
+def get_metadata(entity_id, cycle, entity_type):
+    ''' beginnings of some refactoring. half implemented but
+    harmless. do not pet or feed.'''
+    data = {}
+    # check the metadata to see which of the fields are present. this
+    # determines which sections to display on the entity page.
+    section_indicators = {'individual': {'contributions': ('contributor_amount',),
+                                         'lobbying': ('lobbying_count',)},
+                          'organization' : {'contributions' : ('contributor_amount',),
+                                            'lobbying': ('lobbying_count',)},
+                          'politician' : {'contributions' : ('recipient_amount',)}
+                          }
+
+    entity_info = api.entity_metadata(entity_id, cycle)
+
+    # check which types of data are available about this entity
+    print 'getting metadata for %s %s' % (entity_type, entity_id)
+    for data_type, indicators in section_indicators[entity_type].iteritems():
+        try:
+            print 'totals for %s' % indicators[0]
+            print entity_info['totals'][cycle][indicators[0]]
+        except: pass
+        if (entity_info['totals'].get(cycle, False) and
+            [True for ind in indicators if entity_info['totals'][cycle][ind] ]):
+            data[data_type] = True
+        else:
+            data[data_type] = False
+
+    data['available_cycles'] = entity_info['totals'].keys()
+    # discard the info from cycles that are not the current one
+    if entity_info['totals'].get(cycle, None):
+        entity_info['totals'] = entity_info['totals'][cycle]
+    data['entity_info'] = entity_info
+
+    return data
