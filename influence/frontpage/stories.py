68d177084872bdd516f5df9169ca073ecd2d1b39
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-10-04 18:44:33 -0400

    Final-ish pre-design frontpage code.

--- influence/frontpage/stories.py
+++ influence/frontpage/stories.py
@@ -2,6 +2,8 @@ from influence.frontpage.top_news import TopNews
 from influence import api
 import json
 from influence.frontpage import register_region
+from influence.helpers import generate_label, pie_validate, standardize_organization_name
+from django.template.defaultfilters import striptags
 
 class Stories(TopNews):
     name = 'stories'
@@ -12,34 +14,30 @@ class Stories(TopNews):
     def process_entries(self, pg_entries):
         for entry in pg_entries:
             entry['entities'] = sorted(filter(lambda s: s['tdata_id'] is not None, entry['pg_struct']['entities']), cmp=lambda a, b: cmp(a['relevance'], b['relevance']), reverse=True)
-            entry['text'] = entry['pg_struct']['source_content'].split('</p>')[1] + '</p>'
+            entry['text'] = "<br /><br />".join([striptags(para) for para in entry['pg_struct']['source_content'].split('</p>')[1:3]])
             
         # pick the one with the most entities referenced
-        entry = sorted(pg_entries, cmp=lambda a, b: cmp(len(a['entities']), len(b['entities'])))[0]
+        entry = sorted(pg_entries, cmp=lambda a, b: cmp(len(a['entities']), len(b['entities'])), reverse=True)[0]
+        entry['entities'] = entry['entities'][:2]
         for entity in entry['entities']:
             entity['metadata'] = api.entity_metadata(entity['tdata_id'], cycle='-1')
         
         # de-dupe entities
         seen_so_far = []
-        out_entities = []
         for pg_entity in entry['entities']:
             entity = pg_entity['metadata']
             if entity['id'] not in seen_so_far:
                 if entity['type'] == 'politician':
                     # grab some additional metadata
-                    top_sectors = api.pol_sectors(entity['id'], cycle='-1')
+                    top_sectors = api.pol_industries(entity['id'], cycle='-1')
                     
                     sectors_barchart_data = []
                     for record in top_sectors:
-                        try:
-                            sector_name = catcodes.sector[record['sector']]
-                        except:
-                            sector_name = 'Unknown (%s)' % record['sector']
                         sectors_barchart_data.append({
-                            'key': generate_label(sector_name, max_length=30),
+                            'key': generate_label(standardize_organization_name(record['industry']), max_length=30),
                             'value' : record['amount'],
                         })
-                        entity['sectors_barchart_data'] = json.dumps(sectors_barchart_data[:5])
+                        pg_entity['sectors_barchart_data'] = json.dumps(sectors_barchart_data[:5])
                 
                 elif entity['type'] == 'organization':
                     party_breakdown = api.org_party_breakdown(entity['id'], cycle='-1')
@@ -47,9 +45,8 @@ class Stories(TopNews):
                         party_breakdown[key] = float(values[1])
                     if len(party_breakdown.keys()) < 2:
                         continue
-                    entity['party_breakdown'] = json.dumps(pie_validate(party_breakdown))
+                    pg_entity['party_breakdown'] = json.dumps(pie_validate(party_breakdown))
                 
-                out_entities.append(entity)
                 seen_so_far.append(entity['id'])
         
         out = [entry]
