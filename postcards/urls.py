fe7eabe4b38681128a34f3d96d3870c0b6a3adae
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-10-13 10:31:26 -0400

    Adding blank order page for Ali to add content to.

--- postcards/urls.py
+++ postcards/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('brisket.postcards.views',
     url(r'^$', direct_to_template, {'template': 'postcards/landing.html'}),
-    url(r'^/order$', 'order'),
+    url(r'^/order$', direct_to_template, {'template': 'postcards/order.html'}),
     url(r'^/confirm$', 'confirm'),
     url(r'^/thanks$', 'thanks'),
     url(r'^/hook$', 'hook'),
