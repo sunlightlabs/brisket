2f776bf491fb6410a07c8965b11e7b4711f182de
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-10-13 14:58:11 -0400

    Mostly-working postcard implementation; still needs candidate photos.

--- postcards/management/commands/makepostcards.py
+++ postcards/management/commands/makepostcards.py
@@ -3,9 +3,21 @@ from optparse import make_option
 import os, sys
 import postcards
 from influence import api
+from django.conf import settings
+from django.template.loader import render_to_string
+from gevent.pool import Pool
+from gevent import monkey
+import gevent
+from django.utils.datastructures import SortedDict
+from influence.helpers import standardize_organization_name, standardize_industry_name
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
+        make_option('--photos_only',
+            action='store_true',
+            dest='photos_only',
+            default=False,
+        ),
         make_option('--svg-only',
             action='store_true',
             dest='svg_only',
@@ -15,6 +27,11 @@ class Command(BaseCommand):
             action='store_true',
             dest='rasterize_only',
             default=False,
+        ),
+        make_option('--district',
+            action='store',
+            dest='district',
+            default=None,
         )
     )
     
@@ -26,16 +43,30 @@ class Command(BaseCommand):
         if options['rasterize_only']:
             self.svg = False
         
+        # get ready for gevent
+        monkey.patch_all()
+        
         self.postcard_dir = os.path.dirname(postcards.__file__)
+        self.static_dir = os.path.join(self.postcard_dir, 'static')
         self.svg_dir = os.path.join(self.postcard_dir, 'static', 'svg')
         self.pdf_dir = os.path.join(self.postcard_dir, 'static', 'pdf')
         self.png_dir = os.path.join(self.postcard_dir, 'static', 'png')
         
+        for dir in [self.static_dir, self.svg_dir, self.pdf_dir, self.png_dir]:
+            if not os.path.exists(dir):
+                os.mkdir(dir)
+        
         if self.svg:
+            sys.stdout.write('Fetching districts...\n')
             congress = {'federal:house': {}, 'federal:senate': {}}
-            self.districts = self.get_districts()
-            for district in self.districts:
+            if options['district']:
+                self.districts = [options['district']]
+            else:
+                self.districts = self.get_districts()
+            
+            def get_district(district):
                 state = district.split('-')[0]
+                sys.stdout.write('Fetching candidates for %s...\n' % district)
                 candidates = api.candidates_by_location(district)
                 
                 for chamber in congress.keys():
@@ -48,14 +79,61 @@ class Command(BaseCommand):
                                 candidate = sorted(filter(lambda s: s['name'].endswith(party), members), cmp=lambda a, b: cmp(a['name'], b['name'])).pop(0)
                                 selected.append(candidate)
                             except:
-                                print "Unexpected error:", sys.exc_info()[0]
+                                pass
+                        
+                        # grab these candidates' metadata
+                        for candidate in selected:
+                            sys.stdout.write('Fetching metadata for %s...\n' % candidate['name'])
+                            candidate['entity_info'] = api.entity_metadata(candidate['entity_id'], cycle=settings.LATEST_CYCLE)
+                            candidate['contributions'] = api.pol_contributors(candidate['entity_id'], cycle=settings.LATEST_CYCLE)
+                            candidate['industries'] = api.pol_industries(candidate['entity_id'], cycle=settings.LATEST_CYCLE)
+                            if str(settings.LATEST_CYCLE) in candidate['entity_info']['totals']:
+                                candidate['total'] = candidate['entity_info']['totals'][str(settings.LATEST_CYCLE)]['recipient_amount']
+                            else:
+                                candidate['total'] = 0
+                        
+                        # store the candidates for later use
                         congress[chamber][key] = selected
-            print congress
+            
+            # fire up gevent to fetch them all
+            pool = Pool(10)
+            for district in self.districts:
+                pool.spawn(get_district, district)
+            pool.join()
+            
+            sys.stdout.write('Generating SVGs...\n')
+            for chamber in congress.keys():
+                for loc in congress[chamber].keys():
+                    # single-candidate cards
+                    for candidate in congress[chamber][loc]:
+                        sys.stdout.write('Generating SVG for %s...\n' % candidate['name'])
+                        
+                        # contribution data
+                        contributions = SortedDict()
+                        for cont in candidate['contributions'][:5]:
+                            contributions[standardize_organization_name(cont['name'])] = cont['total_amount']
+                        
+                        # industry data
+                        industries = SortedDict()
+                        for cont in candidate['industries'][:5]:
+                            industries[standardize_industry_name(cont['industry'])] = cont['amount']
+                        
+                        svg_static = os.path.join(self.svg_dir, 'candidate_%s.svg' % candidate['entity_id'])
+                        
+                        out = render_to_string('postcards/single_card.svg', {
+                            'entity_info': candidate['entity_info'],
+                            'contributions': contributions,
+                            'industries': industries,
+                            'district_info': candidate,
+                            'total': int(round(float(candidate['total']))),
+                        })
+                        f = open(svg_static, 'wb')
+                        f.write(out.encode('utf-8'))
+                        f.close()
     
     def get_districts(self):
         # temporary until we have a real API for it
         f = open(os.path.join(self.postcard_dir, 'districts.dat'))
         out = f.read().split('\n')
         f.close()
-        print out
         return out
---
+++ postcards/templatetags/postcard_extras.py
@@ -0,0 +1,82 @@
+from django.template.defaultfilters import stringfilter
+from django import template
+from django.contrib.humanize.templatetags.humanize import ordinal, intcomma
+
+register = template.Library()
+
+@register.filter(name='district_name')
+def district_name(district):
+    if district['seat'] == 'federal:senate':
+        return 'Senate'
+    elif district['seat'] == 'federal:house' and district['district']:
+        d = district['district'].split('-')
+        num = ordinal(int(d[1]))
+        return 'House %s District' % num
+    return ''
+
+@register.filter(name='barchart')
+def barchart(bar_data):
+    # constant sizes of things
+    bar_width = 100
+    label_width = 150
+    dollar_width = 40
+    bar_height = 12
+    bar_spacing = 4
+    pre_bar = 4
+    
+    types = [ellipsize(k, 30) for k in bar_data.keys()]
+    labels = ['$%s' % intcomma(int(float(l))) for l in bar_data.values()]
+    numbers = [float(l) for l in bar_data.values()]
+    
+    ypos = bar_spacing
+    xpos = label_width + pre_bar
+    
+    out = ""
+    
+    for i in range(len(types)):
+        width = bar_width * (float(numbers[i]) / max(numbers))
+        # bar
+        out += '<rect x="%s" y="%s" width="%s" height="%s" style="stroke: none; fill: #efcc01" />' % (
+            xpos,
+            ypos,
+            width,
+            bar_height
+        )
+        
+        # number
+        out += '<text class="legend" x="%s" y="%s">%s</text>' % (
+            xpos + width + 5,
+            ypos + 9,
+            labels[i]
+        )
+        
+        # type label
+        out += '<text class="legend" x="%s" y="%s">%s</text>' % (
+            0,
+            ypos + 9,
+            types[i]
+        )
+        
+        ypos += bar_height + bar_spacing
+    
+    out += '<line x1="%s" y1="%s" x2="%s" y2="%s" style="stroke: #444545; fill: none; stroke-width: 2pt;" />' % (
+        label_width,
+        0,
+        label_width,
+        len(types) * (bar_height + bar_spacing) + bar_spacing + 1.38
+    )
+    
+    out += '<line x1="%s" y1="%s" x2="%s" y2="%s" style="stroke: #444545; fill: none; stroke-width: 2pt;" />' % (
+        label_width,
+        len(types) * (bar_height + bar_spacing) + bar_spacing,
+        label_width + pre_bar + bar_width + dollar_width,
+        len(types) * (bar_height + bar_spacing) + bar_spacing
+    )
+    
+    return out
+
+def ellipsize(val, length):
+    if len(val) > length:
+        return '%s...' % val[:length - 2]
+    else:
+        return val
