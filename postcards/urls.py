7f358136be72e37fbbe702b50e6438f1e661f328
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-10-20 17:06:53 -0400

    Fixing URL redirect; adding social media buttons.

--- postcards/urls.py
+++ postcards/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template, redirect_to
 
 urlpatterns = patterns('brisket.postcards.views',
-    url(r'^$', redirect_to, {'url': '/postcard/order'}),
+    url(r'^/?$', redirect_to, {'url': '/postcard/order'}),
     url(r'^/order$', 'order'),
     url(r'^/thumbnail/(?P<type>\w+)/(?P<id>[\w\-]+)$', 'thumbnail'),
     url(r'^/confirm/(?P<id>\d+)/(?P<hash>\w+)$', 'confirm'),
