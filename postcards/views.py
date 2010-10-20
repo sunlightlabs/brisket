dbbf9c60d580f436eb2a44c3c79299f929c67d9d
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-10-20 10:28:44 -0400

    Adding geolocation and search.

--- postcards/views.py
+++ postcards/views.py
@@ -11,6 +11,9 @@ from django.conf import settings
 import postcards
 import os
 import hashlib
+import urllib2
+import json
+from influence import api
 
 class PostcardForm(forms.ModelForm):
     office = forms.ChoiceField(choices=(('', '---------'), ('house', 'House'), ('senate', 'Senate')), required=False)
@@ -22,15 +25,55 @@ class PostcardForm(forms.ModelForm):
 
     
 def order(request):
+    search_error = False
     if request.method == 'POST':
         form = PostcardForm(request.POST)
         if form.is_valid():
             card = form.save()
             return HttpResponseRedirect('/postcard/confirm/%s/%s' % (card.id, card.get_code()))
     else:
-        form = PostcardForm()
+        # they haven't submitted anything; try search and geolocation
+        latlon = ()
+        initial = {}
+        if 'location' in request.GET:
+            locinfo = json.loads(urllib2.urlopen("http://where.yahooapis.com/geocode?q=%s&flags=J&appid=%s" % (urllib2.quote(request.GET['location']), settings.YAHOO_API_KEY)).read())
+            if 'Results' in locinfo['ResultSet'] and locinfo['ResultSet']['Results']:
+                latlon = (locinfo['ResultSet']['Results'][0]['latitude'], locinfo['ResultSet']['Results'][0]['longitude'])
+            else:
+                search_error = "We're sorry; we were unable to find a location matching your search."
+        else:
+            ipinfo = json.loads(urllib2.urlopen("http://ipinfodb.com/ip_query.php?ip=%s&output=json&timezone=false" % request.META['REMOTE_ADDR']).read())
+            latlon = (ipinfo['Latitude'], ipinfo['Longitude'])
+        
+        if latlon:
+            success = True
+            try:
+                districtinfo = json.loads(urllib2.urlopen("http://services.sunlightlabs.com/api/districts.getDistrictFromLatLong.json?apikey=%s&latitude=%s&longitude=%s" % (settings.API_KEY, latlon[0], latlon[1])).read())
+            except urllib2.HTTPError:
+                success = False
+            if success:
+                state = districtinfo['response']['districts'][0]['district']['state']
+                district = districtinfo['response']['districts'][0]['district']['number']
+                #hack for single-district states
+                if district == 0:
+                    district = 1
+                district = "%02d" % district
+                congress = api.candidates_by_location("%s-%s" % (state, district))
+                house = filter(lambda s: s['seat'] == 'federal:house', congress)
+                inc = filter(lambda s: s['seat_status'] == 'I', house)
+                
+                if inc:
+                    initial = {'office': 'house', 'state': state, 'td_id': inc[0]['entity_id']}
+                elif house:
+                    initial = {'office': 'house', 'state': state, 'td_id': house[0]['entity_id']}
+        
+        if 'location' in request.GET and not initial and not search_error:
+            search_error = "We're sorry; we were unable to locate a politician at that location."
+        
+        
+        form = PostcardForm(initial=initial)
 
-    return direct_to_template(request, 'postcards/order.html', {'form': form,})
+    return direct_to_template(request, 'postcards/order.html', {'form': form, 'search_error': search_error})
 
 def thumbnail(request, type, id):
     img_dir = os.path.join(os.path.dirname(postcards.__file__), 'static', 'png')
