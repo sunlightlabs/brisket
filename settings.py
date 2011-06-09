dbb59e1653e3d566c15d43c1b3c2e7f17bbb7cf6
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-06-09 18:31:45 -0400

    [#859] Remove unnecessary logging config.

--- settings.py
+++ settings.py
@@ -161,41 +161,6 @@ CACHES = {
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['null'],
-            'propagate': True,
-            'level':'INFO',
-        },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'WARN',
-            'propagate': False,
-        },
-    }
-}
 try:
     from local_settings import *
 except:
