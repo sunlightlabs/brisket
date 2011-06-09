f9965049631c73452926915f0a41dda89133f41d
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-06-09 13:36:17 -0400

    [#859] Add logging configuration.

--- data/settings.py
+++ data/settings.py
@@ -156,6 +156,42 @@ MEDIASYNC = {
 # timeout set to a week
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/?timeout=10080'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            'propagate': False,
+        },
+    }
+}
+
 try:
     from local_settings import *
 except ImportError:
