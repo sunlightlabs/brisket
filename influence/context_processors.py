dfa72976138e3e7ed74596e99aaa051fd946db70
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-07-06 18:11:16 -0400

    Adding setting for remote URL, plus new middleware.

--- influence/context_processors.py
+++ influence/context_processors.py
@@ -1,5 +1,6 @@
 from influence.forms import SearchForm
+from django.conf import settings
 
 def custom_context(request):
-    return {'search_form': SearchForm()}
+    return {'search_form': SearchForm(), 'DATA_API_BASE_URL': getattr(settings, "DATA_API_BASE_URL", "http://transparencydata.com/data/")}
     
--- influence/middleware.py
+++ influence/middleware.py
@@ -72,3 +72,8 @@ class RequestLoggingMiddleware():
         return not re.match(r'(^/media|^/simplepay|.*\.ico$)', request.path)
 
 
+class DataRedirectMiddleware(object):
+    def process_request(self, request):
+        if request.META['HTTP_HOST'].startswith('data.') or request.META['SERVER_PORT'] == '8100':
+            request.urlconf = 'brisket.data.urls'
+        return None
