66ad269d0ba2ece23da88b5202cf4a6cd898f5dd
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-07-11 13:29:49 -0400

    removing unused wsgi setup

--- data/django_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import django.core.handlers.wsgi
-
-# Set the django settings and define the wsgi app
-os.environ['DJANGO_SETTINGS_MODULE'] = 'dc_web.settings'
-application = django.core.handlers.wsgi.WSGIHandler()
-
-# Mount the application to the url
-applications = {'/':'application', }
