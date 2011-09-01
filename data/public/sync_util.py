79ca43abb3cf9bc160456b335bd71e7d754a7a4f
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-09-01 11:32:14 -0400

    Porting cache fix over from brisket to TD.

--- data/public/sync_util.py
+++ data/public/sync_util.py
@@ -1,27 +1,30 @@
-import os
-from django.conf import settings
 
 def cache_fix(s):
-    split = s.split(settings.MEDIASYNC['AWS_PREFIX'])
+    from settings import MEDIASYNC, MEDIA_ROOT
+    import os
+
+    split = s.split(MEDIASYNC['AWS_PREFIX'])
     if len(split) < 2:
         return ''
-    rel = s.split(settings.MEDIASYNC['AWS_PREFIX'])[1][1:]
-    rpath = os.path.join(settings.MEDIA_ROOT, rel)
+
+    rel = s.split(MEDIASYNC['AWS_PREFIX'])[1][1:]
+    if rel.endswith('.gz'):
+        rel = rel[:-3]
+    
+    rpath = os.path.join(MEDIA_ROOT, rel)
     paths = []
     if os.path.exists(rpath):
         paths.append(rpath)
     else:
-        JOINED = settings.MEDIASYNC.get('JOINED', {})
-        print rpath, JOINED
+        JOINED = MEDIASYNC.get('JOINED', {})
         if rel in JOINED:
-            paths.extend([os.path.join(settings.MEDIA_ROOT, p) for p in JOINED[rel]])
+            paths.extend([os.path.join(MEDIA_ROOT, p) for p in JOINED[rel]])
     times = []
     for path in paths:
         try:
             times.append(int(os.stat(path).st_mtime))
         except:
             pass
-    print rpath, paths, times
     if times:
         return "t=%s" % max(times)
     else:
