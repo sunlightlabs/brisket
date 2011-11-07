687009d8fa2c57a5c011321a4b8b7f2815d9c270
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-11-07 17:58:25 -0500

    Code style tweaks to brisket.js; the other JS is all spaces and brisket.js was mixed tabs/spaces.

--- media/js/brisket.js
+++ media/js/brisket.js
@@ -1,71 +1,68 @@
 $().ready( function() {
-    $( "input, textarea" ).placehold( );
-});
+    $( "input, textarea" ).placehold();
+    
+    /* hide cycle submit button if JavaScript is enabled */
+    $('#cycle_submit').hide();
+    
+    /* change cycle on select rather than using submit button */
+    $('#id_cycle').bind('change', function() {
+        $('#cycle_form').submit();
+        return false;
+    });
+    
+    /* bind to query input box
+     * on focus: clear the search box if the value equals data-initial
+     * on blur: set value to data-initial if value is empty string
+     */
+    $('#id_query').bind('focus', function() {
+        var q = $(this);
+        if (q.val().toUpperCase() == q.attr('data-initial').toUpperCase()) {
+            q.val('');
+        }
+    }).bind('blur', function() {
+        var q = $(this);
+        if (!q.val()) {
+            q.val(q.attr('data-initial'));
+        }
+    });
+    
+    /* do not submit search form if its value matches data-initial */
+    $('#searchForm').bind('submit', function() {
+        var q = $('#id_query');
+        return q.val() != q.attr('data-initial');
+    });
 
-$().ready(function() {
-	
-	/* hide cycle submit button if JavaScript is enabled */
-	$('#cycle_submit').hide();
-	
-	/* change cycle on select rather than using submit button */
-	$('#id_cycle').bind('change', function() {
-		$('#cycle_form').submit();
-		return false;
-	});
-	
-	/* bind to query input box
-	 * on focus: clear the search box if the value equals data-initial
-	 * on blur: set value to data-initial if value is empty string
-	 */
-	$('#id_query').bind('focus', function() {
-		var q = $(this);
-		if (q.val().toUpperCase() == q.attr('data-initial').toUpperCase()) {
-			q.val('');
-		}
-	}).bind('blur', function() {
-		var q = $(this);
-		if (!q.val()) {
-			q.val(q.attr('data-initial'));
-		}
-	});
-	
-	/* do not submit search form if its value matches data-initial */
-	$('#searchForm').bind('submit', function() {
-		var q = $('#id_query');
-		return q.val() != q.attr('data-initial');
-	});
+    /* section toggles */
+    $('.overviewBar a.toggle').toggle(
+        function() {
+            var bar = $(this).parent();
+            bar.next('.chartModule').slideToggle('slow');
+            bar.addClass("active");
+        }, 
+        function() {
+            var bar = $(this).parent();
+            bar.next('.chartModule').slideToggle('slow');
+            bar.removeClass("active");
+        }
+    );
+    
+    /* descriptor toggles */
+    $("a.descriptor").toggle(
+        function(){
+            var hsh = this.hash;
+            if (hsh[0] != '#') hsh = '#' + hsh;
+            $(this).addClass("active");
+            $(hsh).slideDown("slow");
+        },
+        function(){
+            var hsh = this.hash;
+            if (hsh[0] != '#') hsh = '#' + hsh;
+            $(this).removeClass("active");
+            $(hsh).slideUp("slow");
+        } 
+    );
 
-	/* section toggles */
-	$('.overviewBar a.toggle').toggle(
-		function() {
-			var bar = $(this).parent();
-			bar.next('.chartModule').slideToggle('slow');
-			bar.addClass("active");
-		}, 
-		function() {
-			var bar = $(this).parent();
-			bar.next('.chartModule').slideToggle('slow');
-			bar.removeClass("active");
-		}
-	);
-	
-	/* descriptor toggles */
-	$("a.descriptor").toggle(
-		function(){
-			var hsh = this.hash;
-			if (hsh[0] != '#') hsh = '#' + hsh;
-			$(this).addClass("active");
-			$(hsh).slideDown("slow");
-		},
-		function(){
-			var hsh = this.hash;
-			if (hsh[0] != '#') hsh = '#' + hsh;
-			$(this).removeClass("active");
-			$(hsh).slideUp("slow");
-		} 
-	);
-
-	/* make entity landing pages sortable */
-	$(".sortable").tablesorter({ widgets: ['zebra']}); 
-	
+    /* make entity landing pages sortable */
+    $(".sortable").tablesorter({ widgets: ['zebra']}); 
+    
 });
