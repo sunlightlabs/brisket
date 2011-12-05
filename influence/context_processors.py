6061a909a35b0a47bf26c1df5c30fc5176de5734
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-12-05 14:03:12 -0500

    Adding client API key to IE.

--- influence/context_processors.py
+++ influence/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def custom_context(request):
     return {
         'search_form': SearchForm(),
-        'API_KEY': settings.API_KEY,
+        'API_KEY': getattr(settings, 'CLIENT_API_KEY', settings.API_KEY),
         'AGGREGATES_API_BASE_URL': settings.AGGREGATES_API_BASE_URL
     }
     
