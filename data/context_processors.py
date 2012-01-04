c21c61054b2495553e78c1367d88b3f37509a175
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-01-04 16:21:17 -0500

    Moving the doormat so it appears everywhere, and making it actually work.

--- data/context_processors.py
+++ data/context_processors.py
@@ -7,6 +7,9 @@ def custom_context(request):
         # we're on data.X, so TD urls can be root-relative and IE urls need a full path
         out['IE_BASE_URL'] = getattr(settings, "IE_BASE_URL", "http://influenceexplorer.com/")
         out['DATA_BASE_URL'] = "/"
+        
+        # did we get here via redirect? if so, show doormat
+        out['is_redirect'] = 'redirect' in request.GET or out['IE_BASE_URL'] in request.META.get('HTTP_REFERER', '')
     else:
         # we're on IE, so it's the reverse
         out['IE_BASE_URL'] = "/"
