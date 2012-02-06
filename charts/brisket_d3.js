745d7a6e2bd5a8aba03dba3db7e4d7ca0fa153d6
Andrew Pendleton <abpend@gmail.com>
2012-02-06 00:55:45 -0500

    Strip out most of the redundancy from the chart JS.

--- charts/brisket_d3.js
+++ charts/brisket_d3.js
@@ -567,31 +567,6 @@ BrisketModern = {
 }
 
 BrisketServer = {
-    contribution_single_barchart: function(div, data) {
-        if (data.length === 0) return;
-        
-        graph_data = _.map(data, function(item) {
-            return {
-                key: item.key,
-                href: item.href,
-                values: [parseFloat(item.value)]
-            };
-        });
-
-        D3Charts.barchart(div, graph_data);
-    },
-    contribution_stacked_barchart: function(div, data) {
-        if (data.length === 0) return;
-
-        graph_data = _.map(data, function(item) {
-            return {
-                key: item.key,
-                href: item.href,
-                values: [parseFloat(item.value_employee), parseFloat(item.value_pac)]
-            };
-        });
-        D3Charts.barchart(div, graph_data);
-    },
     piechart: function(div, data) {
         var in_data = []
         var opts = {colors: []}
@@ -600,28 +575,9 @@ BrisketServer = {
             opts.colors.push(value[2]);
         })
         D3Charts.piechart(div, in_data, opts);
-    },
-    party_piechart: function(div, data) {
-        var party_colors = {"Republicans": "#e60002", "Democrats": "#186582", "Other" : "#dcddde"};
-        Brisket.piechart(div, data, party_colors);
-    },
-    indiv_pac_piechart: function(div, data) {
-        var indiv_pac_colors = {"Individuals": '#efcc01', "PACs": '#f2e388'};
-        Brisket.piechart(div, data, indiv_pac_colors);
-    },
-    local_piechart: function(div, data) {
-        var local_colors = {"in-state": '#efcc01', "out-of-state": '#f2e388', "unknown": '#dcddde'};
-        Brisket.piechart(div, data, local_colors);
-    },
-    level_piechart: function(div, data) {
-        var level_colors = {"Federal": '#efcc01', "State": '#f2e388'};
-        Brisket.piechart(div, data, level_colors);
-    },
-    fec_piechart: function(div, data) {
-        var fec_colors = {"Individuals": "#efcc01", "PACs": "#f2e388", "Party": "#15576e", "Self-Financing": "#f2f1e4", "Transfers": "#e96d24"};
-        Brisket.piechart(div, data, fec_colors);
     }
 }
+_.defaults(BrisketServer, BrisketModern);
 
 // base64 function for external links
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(a){var b="";var c,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;a=Base64._utf8_encode(a);while(i<a.length){c=a.charCodeAt(i++);chr2=a.charCodeAt(i++);chr3=a.charCodeAt(i++);enc1=c>>2;enc2=((c&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64}else if(isNaN(chr3)){enc4=64}b=b+this._keyStr.charAt(enc1)+this._keyStr.charAt(enc2)+this._keyStr.charAt(enc3)+this._keyStr.charAt(enc4)}return b},decode:function(a){var b="";var c,chr2,chr3;var d,enc2,enc3,enc4;var i=0;a=a.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<a.length){d=this._keyStr.indexOf(a.charAt(i++));enc2=this._keyStr.indexOf(a.charAt(i++));enc3=this._keyStr.indexOf(a.charAt(i++));enc4=this._keyStr.indexOf(a.charAt(i++));c=(d<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;b=b+String.fromCharCode(c);if(enc3!=64){b=b+String.fromCharCode(chr2)}if(enc4!=64){b=b+String.fromCharCode(chr3)}}b=Base64._utf8_decode(b);return b},_utf8_encode:function(a){a=a.replace(/\r\n/g,"\n");var b="";for(var n=0;n<a.length;n++){var c=a.charCodeAt(n);if(c<128){b+=String.fromCharCode(c)}else if((c>127)&&(c<2048)){b+=String.fromCharCode((c>>6)|192);b+=String.fromCharCode((c&63)|128)}else{b+=String.fromCharCode((c>>12)|224);b+=String.fromCharCode(((c>>6)&63)|128);b+=String.fromCharCode((c&63)|128)}}return b},_utf8_decode:function(a){var b="";var i=0;var c=c1=c2=0;while(i<a.length){c=a.charCodeAt(i);if(c<128){b+=String.fromCharCode(c);i++}else if((c>191)&&(c<224)){c2=a.charCodeAt(i+1);b+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2}else{c2=a.charCodeAt(i+1);c3=a.charCodeAt(i+2);b+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3}}return b}};
@@ -652,27 +608,9 @@ BrisketFallback = {
             }
         })
         BrisketFallback.draw_graph(div, in_data, 'piechart', D3Charts._get_piechart_size(D3Charts.BARCHART_DEFAULTS));
-    },
-    party_piechart: function(div, data) {
-        var party_colors = {"Republicans": "#e60002", "Democrats": "#186582", "Other" : "#dcddde"};
-        Brisket.piechart(div, data, party_colors);
-    },
-    indiv_pac_piechart: function(div, data) {
-        var indiv_pac_colors = {"Individuals": '#efcc01', "PACs": '#f2e388'};
-        Brisket.piechart(div, data, indiv_pac_colors);
-    },
-    local_piechart: function(div, data) {
-        var local_colors = {"in-state": '#efcc01', "out-of-state": '#f2e388', "unknown": '#dcddde'};
-        Brisket.piechart(div, data, local_colors);
-    },
-    level_piechart: function(div, data) {
-        var level_colors = {"Federal": '#efcc01', "State": '#f2e388'};
-        Brisket.piechart(div, data, level_colors);
-    },
-    fec_piechart: function(div, data) {
-        var fec_colors = {"Individuals": "#efcc01", "PACs": "#f2e388", "Party": "#15576e", "Self-Financing": "#f2f1e4", "Transfers": "#e96d24"};
-        Brisket.piechart(div, data, fec_colors);
     }
 }
+_.defaults(BrisketFallback, BrisketModern);
 
 Brisket = BrisketModern;
+// Brisket = BrisketFallback;
