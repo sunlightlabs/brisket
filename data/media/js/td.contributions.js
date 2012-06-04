4d7d5f61f2b06713d68b5ab411dde6ab9bf448af
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2012-06-04 11:56:05 -0700

    Added transaction type description to recipient column.

--- data/media/js/td.contributions.js
+++ data/media/js/td.contributions.js
@@ -5,6 +5,22 @@ $().ready(function() {
     TD.ContributionFilter.path = 'contributions';
     TD.ContributionFilter.ignoreForBulk = ['for_against', 'cycle', 'transaction_namespace'];
     
+    TD.ContributionFilter.transaction_type_description = function(transaction_type) {
+        if (transaction_type == '29') {
+            return "<em>electioneering communication about</em> "
+        }
+        if (transaction_type == '24a') {
+            return "<em>independent expenditure opposing</em> "
+        }
+        if (transaction_type == '24e') {
+            return "<em>independent expenditure supporting</em> "
+        }
+        if (transaction_type.substring(0,2) == '16') {
+            return "<em>loan to</em> "
+        }
+        return ""
+    };
+    
     TD.ContributionFilter.row_content = function(row) {
         var jurisdiction = (row.transaction_namespace == 'urn:fec:transaction') ? 'Federal' : 'State';
         var content = '<td class="jurisdiction">' + jurisdiction + '</td>';
@@ -13,7 +29,7 @@ $().ready(function() {
         content += '<td class="contributor_name">' + row.contributor_name + '</td>';
         content += '<td class="contributor_location">' + TD.Utils.cityStateFormat(row.contributor_city, row.contributor_state) + '</td>';
         content += '<td class="organization_name">' + (row.organization_name || '&nbsp;') + '</td>';
-        content += '<td class="recipient_name">' + row.recipient_name + '</td>';   
+        content += '<td class="recipient_name">' + this.transaction_type_description(row.transaction_type) + row.recipient_name + '</td>';
         return content;
     };
     
