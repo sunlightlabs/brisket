9e3845f42fea444739dccba099f728f43aae9e6d
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-03-11 14:39:54 -0400

    Hack: silently suppress output for party time and the LRT if they error out for any reason.

--- influence/external_sites.py
+++ influence/external_sites.py
@@ -222,8 +222,11 @@ def get_partytime_data(ids):
     if not politician_ids:
         return (None, {'past': [], 'upcoming': []})
       
-    page = urllib2.urlopen("http://politicalpartytime.org/json/%s/" % politician_ids[0]['id'])
-    records = json.loads(page.read())
+    try:
+        page = urllib2.urlopen("http://politicalpartytime.org/json/%s/" % politician_ids[0]['id'])
+        records = json.loads(page.read())
+    except:
+        return None, None
     
     today = datetime.now()
     cutoff = today - timedelta(days=180)
@@ -239,7 +242,10 @@ def get_lobbyist_tracker_data(ids):
     out = []
     tracker_urls = filter(lambda s: s['namespace'] == 'urn:sunlight:lobbyist_registration_tracker_url', ids)
     if tracker_urls:
-        page = urllib2.urlopen("http://reporting.sunlightfoundation.com%s.json" % tracker_urls[0]['id'])
+        try:
+            page = urllib2.urlopen("http://reporting.sunlightfoundation.com%s.json" % tracker_urls[0]['id'])
+        except:
+            return []
         records = json.loads(page.read())
         cutoff = (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d %H:%M:%S')
         out = [record for record in records['registrations'] if record['received'] >= cutoff][:5]
