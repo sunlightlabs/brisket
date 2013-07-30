41d32988a43693de6efde24ad2d9bb625172d6e2
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-07-30 14:37:43 -0400

    Remove the dryrub app from brisket, and add it as an external dependency.

--- dryrub/tests.py
+++
@@ -1,6 +0,0 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
