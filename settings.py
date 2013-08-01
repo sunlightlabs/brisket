79bc267f7cb1c67270a50460a989d367ff73bc8e
Bob Lannon <blannon@gmail.com>
2013-08-01 15:11:14 -0400

    set static root for local dev and added to gitignore

--- settings.py
+++ settings.py
@@ -32,6 +32,7 @@ USE_I18N = False
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
